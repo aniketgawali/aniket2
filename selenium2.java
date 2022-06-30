4f975f93ccacb7cc1e3612315f0715f8ef9e25c5
aniket <M1088974@mindtree.com>
2022-07-01 00:20:59 +0530

    pussing selenium2

---
+++ selenium2.java
@@ -0,0 +1,20 @@
+package com.mindtree.seleniumclass;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selenium2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "./drivers//chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		
+
+	}
+
+}
